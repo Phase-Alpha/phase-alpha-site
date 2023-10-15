7db9bf979d6fb9cadab7813a6dbf5ce53c8605d7
Ahmed Hagi <ahmedosman@mac.com>
2023-10-15 23:05:08 +0100

    parsing works, need to make function async and make test work

--- src/server_functions/posts.rs
+++ src/server_functions/posts.rs
@@ -1,14 +1,16 @@
+#[allow(unused)]
 use chrono::Utc;
-use gray_matter::{engine::TOML, Matter};
+use gray_matter::{engine::YAML, Matter};
 use leptos::*;
-use pulldown_cmark::{html, Parser};
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres, Result}; // for getting the current date
+use pulldown_cmark::{html, Options, Parser};
+use serde::{Deserialize, Serialize};
 use std::fs;
 
 // Define a struct for the metadata
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub struct PostMetadata {
     title: String,
+    description: String,
     date: String,
     image_path: String,
 }
@@ -21,14 +23,15 @@ impl PostMetadata {
 }
 
 // Define a struct representing a blog post
-#[derive(Debug, sqlx::FromRow)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub struct Post {
-    title: String,
+    meta_data: PostMetadata,
     content: String,
 }
 
 // Read Markdown files from a folder and convert them to a vector of posts
-fn read_markdown_files(folder_path: &str) -> Vec<Post> {
+// #[server(GetPosts, "/api")]
+pub fn read_markdown_files(folder_path: &str) -> Vec<Post> {
     let mut posts = Vec::new();
     let mut options = Options::empty();
     options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
@@ -36,38 +39,41 @@ fn read_markdown_files(folder_path: &str) -> Vec<Post> {
         if let Ok(entry) = entry {
             let file_path = entry.path();
             let file_content = fs::read_to_string(&file_path).expect("Error reading file");
-            let matter = Matter::<TOML>::new();
-            let metadata = matter.parse_with_struct::<PostMetadata>(&file_content);
-            let parser = Parser::new_ext(&file_content, &options);
-            let mut title = String::new();
-            let mut content = String::new();
-            let mut date = String::new();
-            // let mut is_title_found = false;
-            // for event in parser {
-            //     match event {
-            //         pulldown_cmark::Event::Text(text) => {
-            //             if !is_title_found {
-            //                 title = text.to_string();
-            //                 is_title_found = true;
-            //             }
-            //             content.push_str(&text.to_string());
-            //         }
-            //         _ => {}
-            //     }
-            // }
-            // Write to String buffer.
-            html::push_html(&mut content, parser);
-            posts.push(Post { title, content });
+            let matter = Matter::<YAML>::new();
+            let metadata = matter
+                .parse_with_struct::<PostMetadata>(&file_content)
+                .expect("Failed to parse front matter");
+            let content = metadata.content;
+            let parser = Parser::new_ext(&content, options);
+            let mut html_content = String::new();
+            html::push_html(&mut html_content, parser);
+            posts.push(Post {
+                meta_data: metadata.data,
+                content: html_content,
+            });
         }
     }
     posts
 }
 
-// Function to retrieve posts from the database
-#[server]
-async fn retrieve_posts(pool: &Pool<Postgres>) -> Result<Vec<Post>> {
-    let posts = sqlx::query_as::<_, Post>("SELECT id, title, content, date FROM posts")
-        .fetch_all(pool)
-        .await?;
-    Ok(posts)
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_POSTS_PATH: &str = "./test-posts/";
+    #[test]
+    fn test_read() {
+        let posts = read_markdown_files(&TEST_POSTS_PATH);
+        let expected = vec![Post {
+            meta_data: PostMetadata {
+                title: String::from("Test Post"),
+                description: String::from("Some testing"),
+                date: String::from("2023-10-15"),
+                image_path: String::from("./public/pic01.jpg"),
+            },
+            content: String::from(
+                "<h1>Test</h1>\n<p>Post content</p>\n<p><img src=\"./public/pic01.jpg\" alt=\"pic\" /></p>\n",
+            ),
+        }];
+        assert_eq!(expected, posts)
+    }
 }
